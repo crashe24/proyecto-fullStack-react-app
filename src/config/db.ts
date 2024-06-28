85633c83ba65faaf078ee950e34f91548553a9b7
iMac 27 <imac27@iMac-Crashe24.local>
2024-06-28 14:03:02 -0500

    change package

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ import  dotenv  from 'dotenv';
 
 dotenv.config()
 const db = new Sequelize('postgres://rest_api_node_typescript_2dk2_user:KYBgLbwp2gmCOvHFBXT9YoWyJyBJCJ7T@dpg-cpd2mhu3e1ms73a59jvg-a.oregon-postgres.render.com/rest_api_node_typescript_2dk2?ssl=true',
-{ models:[__dirname + '/../models/**/*.ts'],
+{ models:[__dirname + '/../models/**/*'],
   logging:false
 
 })
